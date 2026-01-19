39fca0f0e9d73773cb58f63dd4e575ce9ae14891
shubhitiwariiii <shubhitiwari0132@gmail.com>
2026-01-19 21:57:16 +0530

    merge sort

---
+++ sorting/mergeSort.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <vector>
+#include<string>
+using namespace std;
+
+//MERGE SORT
+void merge(int arr[], int si, int mid, int ei){
+    vector<int>temp;
+    int i = si;
+    int j = mid+1;
+    while(i <= mid && j <= ei){
+        if(arr[i]<=arr[j]){
+            temp.push_back(arr[i++]);
+        } else{
+            temp.push_back(arr[j++]);
+        }
+    }
+    while(i<=mid){
+        temp.push_back(arr[i++]);
+    }
+    while(j<=ei){
+        temp.push_back(arr[j++]);
+    }
+
+    //vector element copy in original array
+    for(int idx=si, x=0; idx<=ei; idx++){
+        arr[idx] = temp[x++];
+    }
+}
+
+void mergeSort(int arr[],int si, int ei){
+    if(si >= ei){
+        return;
+    }
+
+    int mid = si + (ei - si) / 2;
+
+    mergeSort(arr, si, mid);//left half
+    mergeSort(arr, mid+1, ei);//right half
+
+    merge(arr, si, mid, ei);//conquer
+}
+
+void printArray(int arr[], int n){
+    for(int i=0; i<n; i++){
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+int main(){
+    int n;
+    cout << "Enter the size of the array: ";
+    cin >> n;
+
+    int arr[n];
+    cout << "Enter the elements of the array: ";
+    for(int i=0; i<n; i++){
+        cin >> arr[i];
+    }
+
+    mergeSort(arr, 0, n-1);
+    printArray(arr, n);
+
+    return 0;
+}
