96ff1fc4e503194db07d978770a28a4dc9cac09b
shubhitiwariiii <shubhitiwari0132@gmail.com>
2026-01-23 22:19:08 +0530

    fibonacci number

---
+++ Recursion/fibonaciiNumber.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int fibonacci(int n) {
+    if (n <= 1) {
+        return n;
+    }
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+int main() {
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+    cout << "Fibonacci number at position " << n << " is: " << fibonacci(n) << endl;
+    return 0;
+}
