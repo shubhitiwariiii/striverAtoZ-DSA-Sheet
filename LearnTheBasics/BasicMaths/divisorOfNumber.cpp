2da387f0b23666070d627b83a0ada3948a54547c
shubhitiwariiii <shubhitiwari0132@gmail.com>
2026-03-01 14:06:42 +0530

    optimized

--- LearnTheBasics/BasicMaths/divisorOfNumber.cpp
+++ LearnTheBasics/BasicMaths/divisorOfNumber.cpp
@@ -1,13 +1,13 @@
 //Ques: You are given an integer n. You need to find all the divisors of n. Return all the divisors of n as an array or list in a sorted order.
 // A number which completely divides another number is called it's divisor.
+
+
 #include<bits/stdc++.h>
 using namespace std;
 
-
-
 void printDivisors(int n){
     vector<int> ls;
-    for(int i=1; i<sqrt(n); i++){
+    for(int i=1; i*i<=n; i++){
         if(n%i == 0) {
             ls.push_back(i);
             if(n/i != i){
