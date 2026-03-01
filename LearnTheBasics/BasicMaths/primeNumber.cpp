c852f0015ae208842269f6baedab0d40cfee21ee
shubhitiwariiii <shubhitiwari0132@gmail.com>
2026-03-01 15:01:31 +0530

    optimized approach

--- LearnTheBasics/BasicMaths/primeNumber.cpp
+++ LearnTheBasics/BasicMaths/primeNumber.cpp
@@ -1,41 +1,49 @@
 //Ques: You are given an integer n. You need to check if the number is prime or not. Return true if it is a prime number, otherwise return false.
 // A prime number is a number which has no divisors except 1 and itself.
 
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
 
-bool checkPrime(int n){
-    int cnt = 0;
+// bool checkPrime(int n){
+//     int cnt = 0;
 
-    for(int i=1; i<=n; i++){
-        if(n % i == 0){
-            cnt++;
-        }
-    }
+//     for(int i=1; i<=n; i++){
+//         if(n % i == 0){
+//             cnt++;
+//         }
+//     }
+//     if(cnt == 2){
+//         return true;
+//     }
+//     else{
+//         return false;
+//     }
+// }
 
-    if(cnt == 2){
-        return true;
-    }
-
-    else{
-        return false;
-    }
-
-}
 int main(){
     int n;
     cout << "Enter the number: ";
     cin >> n;
 
-    bool isPrime = checkPrime(n);
+    // bool isPrime = checkPrime(n);
+    // if(isPrime){
+    //     cout << n << " is a prime number."<< endl;
+    // }
+    // else{
+    //      cout << n << " is not a prime number."<< endl;
+    // }
 
-    if(isPrime){
-        cout << n << " is a prime number."<< endl;
+    int cnt = 0;
+    for(int i=1; i*i<=n; i++){
+        if(n%i == 0) {
+            cnt++;
+            if(n/i != i){
+               cnt++;
+            }    
+        }
     }
-
-    else{
-         cout << n << " is not a prime number."<< endl;
-    }
+    if(cnt == 2) cout << "true";
+    else cout << "false";
     
     return 0;
 }
