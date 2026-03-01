d9b7d1d9faaa186161f9490738ca31f05af6d7dd
shubhitiwariiii <shubhitiwari0132@gmail.com>
2026-03-01 23:03:37 +0530

    countArrayFreq

--- LearnTheBasics/hashing/countArrayFreq.cpp
+++ LearnTheBasics/hashing/countArrayFreq.cpp
@@ -1,6 +1,4 @@
-#include<iostream>
-#include<unordered_map>
-#include<vector>
+#include<bits/stdc++.h>
 using namespace std;
 
 void freq(int arr[], int n){
@@ -12,9 +10,76 @@ void freq(int arr[], int n){
         cout<<it.first<<" "<<it.second<<endl;
     }
 }
+
 int main(){
-    int arr[] = {1, 2, 2, 3, 4, 4, 4, 5};
-    int n = sizeof(arr)/sizeof(arr[0]);
+    int n;
+    cout << "enter size of array: ";
+    cin >> n;
+    int arr[n];
+    cout << "enter the elements of array:";
+    for(int i=0; i<n; i++){
+        cin >> arr[i];
+    }
+    
+    // // precomput
+    // int hash[13] = {0};
+    // for(int i=0; i<n; i++){
+    //     hash[arr[i]] += 1;
+    // }
+    
+    // int q;
+    // cin >> q;
+    // while(q--){
+    //     int num;
+    //     cin >> num; 
+    //     //fetch
+    //     cout << hash[num] <<"->";
+    // }
+
+    // string s;
+    // cin >> s;
+
+    // //precompute
+    // int hash[26] = {0};
+    // for(int i=0; i<s.size(); i++){
+    //     hash[s[i] - 'a']++;
+    // }
+
+    // int q;
+    // cin >> q;
+    // while(q--){
+    //     char c;
+    //     cin >> c;
+    //     //fetch
+    //     cout << hash[c-'a'];
+    // }
+
+
+    // int n;
+    // cout << "enter size of array: ";
+    // cin >> n;
+    // int arr[n];
+    // cout << "enter the elements of array:";
+    // map<int, int> mpp;
+    // for(int i=0; i<n; i++){
+    //     cin >> arr[i];
+    //     mpp[arr[i]]++;
+    // }
+    // //precompute
+    
+    // for(auto it : mpp){
+    //     cout << it.first <<"->" << it.second << endl;
+    // }
+
+    // int q;
+    // cin >> q;
+    // while(q--){
+    //     int num;
+    //     cin >> num; 
+    //     //fetch
+    //     cout << mpp[num] << endl;
+    // }
+
     freq(arr, n);
     return 0;
 }
