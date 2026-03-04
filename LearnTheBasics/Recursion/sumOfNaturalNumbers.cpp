b13be11a7713b18d7a06b99e6912297ead6879c5
shubhitiwariiii <shubhitiwari0132@gmail.com>
2026-03-04 11:00:14 +0530

    sum using functional recursion

--- LearnTheBasics/Recursion/sumOfNaturalNumbers.cpp
+++ LearnTheBasics/Recursion/sumOfNaturalNumbers.cpp
@@ -2,18 +2,26 @@
 using namespace std;
 
 
-void sumOfNaturalNumber(int i, int sum){
-    if(i<1){
-        cout << sum;
-        return;
+// void sumOfNaturalNumber(int i, int sum){
+//     if(i<1){
+//         cout << sum;
+//         return;
+//     }
+//     sumOfNaturalNumber(i-1, sum+i);
+// }
+
+int sum(int n){
+    if(n == 0){
+        return 0;
     }
-    sumOfNaturalNumber(i-1, sum+i);
+    return n + sum(n-1);
 }
 int main(){
     int n;
     cout << "enter the number: ";
     cin >> n;
-    sumOfNaturalNumber(n,0);
+    // sumOfNaturalNumber(n,0);
+    cout << "sum is: " << sum(n);
 
     return 0;
 }
