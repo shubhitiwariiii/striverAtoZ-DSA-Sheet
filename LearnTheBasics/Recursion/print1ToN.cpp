521ec2aa5628be17f3cc1151751c039940cbb75a
shubhitiwariiii <shubhitiwari0132@gmail.com>
2026-03-04 10:16:08 +0530

    print numbers from 1 to N

--- LearnTheBasics/Recursion/print1ToN.cpp
+++ LearnTheBasics/Recursion/print1ToN.cpp
@@ -1,26 +1,20 @@
-// Given an integer n, write a function to print all numbers from 1 to n (inclusive) using recursion.
-// You must not use any loops such as for, while, or do-while.
-// The function should print each number on a separate line, in increasing order from 1 to n.
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-void print1ToN(int n) {
+void print1ToN(int i , int n) {
     // Base case: if n is 0, return
-    if (n == 0) {
+    if (i>n) {
         return;
     }
-    // Recursive call with n-1
-    print1ToN(n - 1);
-    // Print the current number after the recursive call
-    cout << n << endl;
+    cout << i << endl;
+    print1ToN(i + 1, n);
+    
 }
 int main() {
     int n;
-    cout << "Enter a positive integer n: ";
     cin >> n;
 
-    cout << "Printing numbers from 1 to " << n << ":\n";
-    print1ToN(n);
+    print1ToN(1,n);
 
     return 0;
 }
