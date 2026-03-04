4d9cd99b5466837ec16a42a287405faef32b498e
shubhitiwariiii <shubhitiwari0132@gmail.com>
2026-03-04 11:38:27 +0530

    factorial

--- LearnTheBasics/Recursion/factorialUsingRecursion.cpp
+++ LearnTheBasics/Recursion/factorialUsingRecursion.cpp
@@ -1,23 +1,13 @@
-//ques1 : You are given an integer n. Return the value of n! or n factorial.
-// Factorial of a number is the product of all positive integers less than or equal to that number.
-#include <iostream>
+#include<bits/stdc++.h>
 using namespace std;
 
-int factorialUsingRecursion(int n) {
-    // Base case: if n is 0 or 1, the factorial is 1
-    if (n == 0 || n == 1) {
-        return 1;
-    }
-    // Recursive case: n! = n * (n-1)!
-    return n * factorialUsingRecursion(n - 1);
+int fact(int n){
+    if(n == 0 || n == 1) return 1;
+    return n * fact(n-1);
 }
-int main() {
+int main(){
     int n;
-    cout << "Enter a positive integer N: ";
+    cout << "enter the number: ";
     cin >> n;
-
-    int factorial = factorialUsingRecursion(n);
-    cout << "The factorial of " << n << " is: " << factorial << endl;
-
-    return 0;
+    cout << fact(n);
 }
