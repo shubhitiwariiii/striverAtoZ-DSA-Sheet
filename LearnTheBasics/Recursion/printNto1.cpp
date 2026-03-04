32bb7ace511c44bd70e8aec9f53ca2e9c48f50c2
shubhitiwariiii <shubhitiwari0132@gmail.com>
2026-03-04 10:53:34 +0530

    sum of natural number by parameterized recursion

--- LearnTheBasics/Recursion/printNto1.cpp
+++ LearnTheBasics/Recursion/printNto1.cpp
@@ -9,11 +9,13 @@ void printNto1(int i, int n){
     cout << i << endl;
     printNto1(i-1,n);
 }
+
 int main(){
     int n;
     cout << "enter the number: ";
     cin >> n;
     printNto1(n,n);
+    
 
     return 0; 
 }
--- LearnTheBasics/Recursion/sumOfNaturalNumbers.cpp
+++ LearnTheBasics/Recursion/sumOfNaturalNumbers.cpp
@@ -1,22 +1,19 @@
-// Given an integer N, return the sum of first N natural numbers. Try to solve this using recursion.
-#include <iostream>
+#include<bits/stdc++.h>
 using namespace std;
 
-int sumOfFirstNNaturalNumbers(int n) {
-    // Base case: if n is 0, the sum is 0
-    if (n == 0) {
-        return 0;
-    }
-    // Recursive case: sum of first n natural numbers is n + sum of first (n-1) natural numbers
-    return n + sumOfFirstNNaturalNumbers(n - 1);
-}
-int main() {
-    int n;
-    cout << "Enter a positive integer N: ";
-    cin >> n;
 
-    int sum = sumOfFirstNNaturalNumbers(n);
-    cout << "The sum of the first " << n << " natural numbers is: " << sum << endl;
+void sumOfNaturalNumber(int i, int sum){
+    if(i<1){
+        cout << sum;
+        return;
+    }
+    sumOfNaturalNumber(i-1, sum+i);
+}
+int main(){
+    int n;
+    cout << "enter the number: ";
+    cin >> n;
+    sumOfNaturalNumber(n,0);
 
     return 0;
 }
