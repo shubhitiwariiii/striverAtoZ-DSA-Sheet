7c07f7f4f1a312d3c1b63d2bac1331f8919dbd93
shubhitiwariiii <shubhitiwari0132@gmail.com>
2026-03-04 10:37:22 +0530

    another approach

--- LearnTheBasics/Recursion/print1ToN.cpp
+++ LearnTheBasics/Recursion/print1ToN.cpp
@@ -1,20 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void print1ToN(int i , int n) {
-    // Base case: if n is 0, return
-    if (i>n) {
+// void print1ToN(int i , int n) {
+//     // Base case: if n is 0, return
+//     if (i>n) {
+//         return;
+//     }
+//     cout << i << endl;
+//     print1ToN(i + 1, n);
+    
+// }
+void print1toN(int i, int n){
+    //base case
+    if(i<1){
         return;
     }
+    print1toN(i-1,n);
     cout << i << endl;
-    print1ToN(i + 1, n);
-    
 }
 int main() {
     int n;
     cin >> n;
 
-    print1ToN(1,n);
+    print1toN(n,n);
 
     return 0;
 }
