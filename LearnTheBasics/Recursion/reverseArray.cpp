02be8ab818e11547ad6d31136919f051c11dd55b
shubhitiwariiii <shubhitiwari0132@gmail.com>
2026-03-04 16:35:24 +0530

    removing duplicate codes

--- LearnTheBasics/Recursion/reverseArray.cpp
+++ LearnTheBasics/Recursion/reverseArray.cpp
@@ -1,32 +1,3 @@
-// #include<bits/stdc++.h>
-// using namespace std;
-
-// void reverseArr(int i,int a[], int n){
-//     if(i >= n/2){
-//         return;
-//     }
-//     swap(a[i] , a[n-i-1]);
-//     reverseArr(i+1,a, n);
-// }
-// int main(){
-//     int n;
-//     cout << "size of arr: ";
-//     cin >> n;
-
-//     int a[n];
-
-//     for(int i=0; i<n; i++){
-//         cin >> a[i];
-//     }
-
-//     reverseArr(0,a, n);
-    
-//     for(int i=0; i<n; i++){
-//         cout << a[i] << " ";
-//     }
-//     return 0;
-// }
-
 #include<bits/stdc++.h>
 using namespace std;
 
