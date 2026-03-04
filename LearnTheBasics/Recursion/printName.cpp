e29fc0d9346864292906668a64c350ac6e960ba8
shubhitiwariiii <shubhitiwari0132@gmail.com>
2026-03-04 10:21:41 +0530

    print number from N to 1

--- LearnTheBasics/Recursion/printName.cpp
+++ LearnTheBasics/Recursion/printName.cpp
@@ -1,6 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+//TC : O(n), SC: O(n)
 void fname(int i, int n){
     if(i > n){
         return;
--- LearnTheBasics/Recursion/printNto1.cpp
+++ LearnTheBasics/Recursion/printNto1.cpp
@@ -1,26 +1,19 @@
-// Given an integer n, write a function to print all numbers from n to 1 (inclusive) using recursion.
-// You must not use any loops such as for, while, or do-while.
-// The function should print each number on a separate line, in decreasing order from n to 1
-#include <iostream>
+#include<bits/stdc++.h>
 using namespace std;
 
-void printNto1(int n) {
-    // Base case: if n is 0, return
-    if (n == 0) {
+void printNto1(int i, int n){
+    //base case
+    if(i<1){
         return;
     }
-    // Print the current number before the recursive call
-    cout << n << endl;
-    // Recursive call with n-1
-    printNto1(n - 1);
+    cout << i << endl;
+    printNto1(i-1,n);
 }
-int main() {
+int main(){
     int n;
-    cout << "Enter a positive integer n: ";
+    cout << "enter the number: ";
     cin >> n;
+    printNto1(n,n);
 
-    cout << "Printing numbers from " << n << " to 1:\n";
-    printNto1(n);
-
-    return 0;
+    return 0; 
 }
