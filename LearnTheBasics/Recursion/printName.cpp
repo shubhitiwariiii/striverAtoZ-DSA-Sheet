0f68c729c40eb84ac7de8c972d67827ddebba9dc
shubhitiwariiii <shubhitiwari0132@gmail.com>
2026-03-04 10:04:42 +0530

    print name

---
+++ LearnTheBasics/Recursion/printName.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void fname(int i, int n){
+    if(i > n){
+        return;
+    }
+    cout << "shubhi" << endl;
+    fname(i+1, n);
+}
+int main(){
+    int n;
+    cin >> n;
+    fname(1,n);
+
+    return 0;
+}
