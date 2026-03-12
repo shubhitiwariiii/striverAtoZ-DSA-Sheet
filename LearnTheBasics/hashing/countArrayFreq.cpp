fcfdb728682e3a3f963bbbcb0b520d868807f0ba
shubhitiwariiii <shubhitiwari0132@gmail.com>
2026-03-12 09:25:31 +0530

    countArrayFreq

--- LearnTheBasics/hashing/countArrayFreq.cpp
+++ LearnTheBasics/hashing/countArrayFreq.cpp
@@ -1,27 +1,27 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void freq(int arr[], int n){
-    unordered_map<int, int>map;
-    for(int i=0; i<n; i++){
-        map[arr[i]]++;
-    }
-    for(auto it: map){
-        cout<<it.first<<" "<<it.second<<endl;
-    }
-}
+// void freq(int arr[], int n){
+//     unordered_map<int, int>map;
+//     for(int i=0; i<n; i++){
+//         map[arr[i]]++;
+//     }
+//     for(auto it: map){
+//         cout<<it.first<<" "<<it.second<<endl;
+//     }
+// }
 
 int main(){
-    int n;
-    cout << "enter size of array: ";
-    cin >> n;
-    int arr[n];
-    cout << "enter the elements of array:";
-    for(int i=0; i<n; i++){
-        cin >> arr[i];
-    }
-    
-    // // precomput
+    // int n;
+    // cout << "enter size of array: ";
+    // cin >> n;
+    // int arr[n];
+    // cout << "enter the elements of array:";
+    // for(int i=0; i<n; i++){
+    //     cin >> arr[i];
+    // }
+
+    // // precompute
     // int hash[13] = {0};
     // for(int i=0; i<n; i++){
     //     hash[arr[i]] += 1;
@@ -36,13 +36,21 @@ int main(){
     //     cout << hash[num] <<"->";
     // }
 
+
     // string s;
     // cin >> s;
 
-    // //precompute
+    //precompute
+    //for lowercase letters
     // int hash[26] = {0};
     // for(int i=0; i<s.size(); i++){
     //     hash[s[i] - 'a']++;
+    // }
+
+    //for any case
+    // int hash[256] = {0};
+    // for(int i=0; i<s.size(); i++){
+    //     hash[s[i]]++;
     // }
 
     // int q;
@@ -50,36 +58,41 @@ int main(){
     // while(q--){
     //     char c;
     //     cin >> c;
+
     //     //fetch
     //     cout << hash[c-'a'];
     // }
 
 
-    // int n;
-    // cout << "enter size of array: ";
-    // cin >> n;
-    // int arr[n];
-    // cout << "enter the elements of array:";
-    // map<int, int> mpp;
-    // for(int i=0; i<n; i++){
-    //     cin >> arr[i];
-    //     mpp[arr[i]]++;
-    // }
-    // //precompute
+
+    int n;
+    cout << "enter size of array: ";
+    cin >> n;
+    int arr[n];
+    cout << "enter the elements of array:";
+
+    map<int, int> mpp;
+
+    for(int i=0; i<n; i++){
+        cin >> arr[i];
+        mpp[arr[i]]++;
+    }
+
+    //precompute
     
-    // for(auto it : mpp){
-    //     cout << it.first <<"->" << it.second << endl;
-    // }
+    for(auto it : mpp){
+        cout << it.first <<"->" << it.second << endl;
+    }
 
-    // int q;
-    // cin >> q;
-    // while(q--){
-    //     int num;
-    //     cin >> num; 
-    //     //fetch
-    //     cout << mpp[num] << endl;
-    // }
+    int q;
+    cin >> q;
+    while(q--){
+        int num;
+        cin >> num; 
+        //fetch
+        cout << mpp[num] << endl;
+    }
 
-    freq(arr, n);
+    // freq(arr, n);
     return 0;
 }
--- LearnTheBasics/hashing/highestLowestFreqEle.cpp
+++ LearnTheBasics/hashing/highestLowestFreqEle.cpp
@@ -1,9 +1,7 @@
 //Ques1: Given an array nums of n integers, find the most frequent element in it i.e., the element that occurs the maximum number of times. If there are multiple elements that appear a maximum number of times, find the smallest of them.
 // Please note that this section might seem a bit difficult without prior knowledge on what hashing is, we will soon try to add basics concepts for your ease! If you know the concepts already please go ahead to give a shot to the problem. Cheers!
 
-#include<iostream>
-#include<unordered_map>
-#include<vector>
+#include<bits/stdc++.h>
 using namespace std;
 
 int highestFreqEle(vector<int> &arr){
