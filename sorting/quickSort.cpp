4f8a86a232f3ae6dfd6e27ab24bdb1edcc045e43
shubhitiwariiii <shubhitiwari0132@gmail.com>
2026-03-14 11:08:41 +0530

    quick sort

--- sorting/quickSort.cpp
+++ sorting/quickSort.cpp
@@ -1,38 +1,6 @@
-// Given an array of integers called nums, sort the array in non-decreasing order using the quick sort algorithm and return the sorted array.
-//A sorted array in non-decreasing order is an array where each element is greater than or equal to all preceding elements in the array.
-
-#include <iostream>
-#include <vector>
-#include<string>
+#include<bits/stdc++.h>
 using namespace std;
 
-//QUICK SORT
-int partition(int arr[], int si, int ei){
-    int i = si - 1;
-    int pivot = arr[ei];
-
-    for(int j=si; j<ei; j++){
-        if(arr[j] <= pivot){
-            i++;
-            swap(arr[i],arr[j]);
-        }
-    }
-    i++;
-    swap(arr[i],arr[ei]);
-    //pivotIdx = i
-    return i;
-}
-
-void quickSort(int arr[], int si, int ei){
-    if(si>=ei){
-        return;
-    }
-     
-    int pivotIdx = partition(arr,si,ei);
-
-    quickSort(arr,si,pivotIdx-1);
-    quickSort(arr,pivotIdx+1,ei);
-}
 
 void printArray(int arr[],int n){
     for(int i=0; i<n; i++){
@@ -40,6 +8,33 @@ void printArray(int arr[],int n){
     }
     cout << endl;
 }
+
+int partition(int arr[], int low, int high){
+    int pivot = arr[low];
+    int i = low, j = high;
+
+    while(i < j){
+        while(arr[i] <= pivot && i < high){
+            i++;
+        }
+        while(arr[j] >= pivot && j > low){
+            j--;
+        }
+        if(i<j) swap(arr[i], arr[j]);
+    }
+    swap(arr[low], arr[j]);
+    return j;
+}
+
+void quickSort(int arr[],int low, int high){
+    if(low<high){
+        int pIdx = partition(arr,low,high);
+        quickSort(arr,low,pIdx-1);
+        quickSort(arr,pIdx+1, high);
+    }
+}
+
+
 
 int main(){
     int n;
